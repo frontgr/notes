2764076a51e5529189de4bf34182cf51f8a94289
nibezo <nibezo.cs@gmail.com>
2023-11-24 02:45:59 +0400

    feat: Create localy working note-taking app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const notes = ref([]);
+const newNote = ref("");
+newNote.value = "";
+const addNote = () => {
+  if (newNote.value) {
+    notes.value.push(newNote.value);
+    newNote.value = "";
+  }
+};
+</script>
 
 <template>
-  <header></header>
-  <main></main>
+  <div>
+    <textarea v-model="newNote" placeholder="Enter a note"></textarea>
+    <button @click="addNote">Add Note</button>
+    <ul>
+      <li v-for="note in notes" :key="note">{{ note }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
