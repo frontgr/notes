6b06704d6c720cbee686f83e5811f742a2611bac
aryzhkov <ryzhkov.anton.yu@gmail.com>
2023-11-28 21:10:23 +0400

    fix: Add CORS policy

--- NotesFlask/NotesFlaskApp.py
+++ NotesFlask/NotesFlaskApp.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import json
+from flask_cors import CORS
 import MongoAccess as monots
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/get-notes', methods=['GET'])
